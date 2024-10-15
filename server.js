f4f25891be7bc2275ef8bb832bd7d57e5ab4f2e3
Alirayyan001 <160483534+Alirayyan001@users.noreply.github.com>
2024-10-16 00:52:00 +0500

    Server.js
    Server.js file Backend

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const announcementsRouter = require('./routes/announcements');
+const authRouter = require('./routes/auth');
+const topupRouter = require('./routes/topup');
+const walletRouter = require('./routes/wallet');
+
+
+
+// Load config
+dotenv.config({ path: './.env' });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRouter);
+app.use('/api/announcements', announcementsRouter);
+app.use('/api/topup', topupRouter);
+app.use('/api/wallet', walletRouter);
+
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
